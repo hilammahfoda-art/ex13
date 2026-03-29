2c848f7715ac7826406045471cbb0475bdbb14a6
hilammahfoda-art <hilammahfoda@gmail.com>
2026-03-29 12:06:00 +0300

    commit 2

--- app/src/main/java/com/example/ex13/MainActivity.java
+++ app/src/main/java/com/example/ex13/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.ex13;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+/**
+ * MainActivity handles the logic for changing the background color.
+ * It supports instant changes via RadioGroup or delayed changes via Button,
+ * depending on the Switch state.
+ *
+ * @author [Hila mahfoda]
+ * @version 1.0
+ */
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,9 +29,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
         });
     }
 }
